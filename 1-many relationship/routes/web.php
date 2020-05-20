d00d12dee6d9ef693d0a4432ef5d7a83d688aa94
R-O-M-CODE <rafiumoshoodolakunle@gmail.com>
2020-05-20 14:08:03 +0100

    1-many relationship

--- 1-many relationship/routes/web.php
+++ 1-many relationship/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\User;
+use App\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Inserting in database
+
+Route::get('/insert', function(){
+
+    $user = User::findOrFail(1);
+
+    $post = new Post(['title'=>'New Title Post', 'body' => 'I love laravel']);
+
+    $user->posts()->save($post);
+});
+
+// read from database
+Route::get('/read', function(){
+    $user = User::findOrFail(1);
+// You have to use foreach if its not a one-one relationship
+    foreach ($user->posts as $post){
+        echo $post->title."<br/>";
+    }
+
+});
+
+// Update database
+Route::get('/update', function(){
+    $user = User::findOrFail(1);
+
+    $user->posts()->whereUserId(1)->update(['title'=>'LOving Laravel', 'body'=> 'Laravel is my babay']);
+});
+
+// Deleing from database
+
+Route::get('/delete', function(){
+    $user = USer::findOrFail(1);
+    // since the soft delete isnt installed, the delete method
+    // deletes the item permanently, else we use the forcedelete() methos
+    $user->posts()->whereId(4)->delete();
 });
