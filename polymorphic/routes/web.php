e865b913750ca736bb63ec6ec19d70460148897f
R-O-M-CODE <rafiumoshoodolakunle@gmail.com>
2020-05-20 23:32:24 +0100

    Polymorpic relationship

--- polymorphic/routes/web.php
+++ polymorphic/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Staff;
+use App\Photo;
+use App\Product;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +17,57 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Insert
+
+Route::get('/create', function(){
+    $staff = Staff::findOrFail(1);
+
+    $staff->photos()->create(['path'=>'example.png']);
+});
+
+// Read
+Route::get('/read', function(){
+    $staff = Staff::findOrFail(1);
+
+    foreach ($staff->photos as $photo){
+        return $photo->path;
+    }
+});
+
+// Update
+
+Route::get('/update', function(){
+    $staff = Staff::findOrFail(1);
+
+    $photo = $staff->photos()->whereId(1)->first();
+
+    $photo->path = "second.jpg";
+
+    $photo->save();
+});
+
+// Delete
+
+Route::get('/delete', function(){
+    $staff = Staff::findOrFail(1);
+
+    $staff->photos()->delete();
+});
+
+//Extra Techniques
+
+Route::get('/assign', function(){
+    $staff = Staff::findOrFail(1);
+
+    $photo = Photo::findOrFail(5);
+
+    $staff->photos()->save($photo);
+});
+
+Route::get('/unassign', function(){
+    $staff = Staff::findOrFail(1);
+
+    $staff->photos()->whereId(4)->update(['imageable_id'=>0, 'imageable_type'=>'']);
 });
