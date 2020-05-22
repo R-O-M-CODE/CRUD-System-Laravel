5fa433525efa3ea951d1b47f552134f9912c0021
R-O-M-CODE <rafiumoshoodolakunle@gmail.com>
2020-05-22 16:27:03 +0100

    User Aythentication System

--- login/routes/web.php
+++ login/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+//    if (Auth::check()){
+//        return "The user is logged in";
+//    }
+//    $username = 'dvhcbi';
+//    $password = 'fdgcd';
+//    if (Auth::attempt(['username' => $username, 'passsword'=> $password])){
+//        return redirect()->intended('/admin');
+//    }
+
+    Auth::logout();
+
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
